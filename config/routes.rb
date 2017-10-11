4c4a519534bd1ff320355ed163c8384491f57a82
cam <cam.marshall940@yahoo.com>
2017-10-11 15:46:34 -0500

    Fixed spacing

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'about', to: "static_pages#about"
 	get 'contact', to: "static_pages#contact"
+	get 'live', to: "static_pages#live"
 
   root :to => 'home#index'
 
